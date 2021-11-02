8046d6bc3fa413f7c7f25e6e1838e4024c55a48c
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-11-02 22:25:06 +0000

    Match API, fix plot.

--- examples/inflation_example.py
+++ examples/inflation_example.py
@@ -13,14 +13,9 @@ print(host)
 # Define calculator object.
 calculator = rg.Inflater(
     step_size=0.1,
-    rotation_step_size=1,
     bead_sigma=1.5,
-    max_size_modifier=1,
-    max_beads=100,
-    num_dynamics_steps=100,
-    nonbond_epsilon=1,
-    beta=3,
-    random_seed=1000,
+    num_beads=100,
+    num_steps=100,
 )
 
 # Run calculator on host object, analysing output.
@@ -43,7 +38,9 @@ for step_result in calculator.inflate_blob(host=host):
     )
     blob_properties[step_result.step] = {
         'num_movable_beads': step_result.num_movable_beads,
-        'max_diam': step_result.blob.get_maximum_diameter(),
+        'blob_max_diam': step_result.blob.get_maximum_diameter(),
+        'pore_max_diam': step_result.pore.get_maximum_distance_to_com(),
+        'pore_mean_diam': step_result.pore.get_mean_distance_to_com(),
     }
 
 # Do a plot of properties.
@@ -53,17 +50,39 @@ ax[0].plot(
     [
         blob_properties[i]['num_movable_beads'] for i in blob_properties
     ],
+    c='k',
+    lw=2,
 )
 ax[0].tick_params(axis='both', which='major', labelsize=16)
-ax[0].set_ylabel('num_movable_beads', fontsize=16)
+ax[0].set_ylabel('num. movable beads', fontsize=16)
 
 ax[1].plot(
     [i for i in blob_properties],
-    [blob_properties[i]['max_diam'] for i in blob_properties],
+    [blob_properties[i]['blob_max_diam'] for i in blob_properties],
+    c='#DC267F',
+    label='blob',
+    lw=2,
+)
+ax[1].plot(
+    [i for i in blob_properties],
+    [blob_properties[i]['pore_max_diam'] for i in blob_properties],
+    c='#785EF0',
+    label='pore max',
+    lw=2,
+)
+ax[1].plot(
+    [i for i in blob_properties],
+    [blob_properties[i]['pore_mean_diam'] for i in blob_properties],
+    c='#648FFF',
+    label='pore mean',
+    lw=2,
+    linestyle='--'
 )
 ax[1].tick_params(axis='both', which='major', labelsize=16)
+ax[1].set_xlim(0, None)
 ax[1].set_xlabel('step', fontsize=16)
-ax[1].set_ylabel('max. diameter [AA]', fontsize=16)
+ax[1].set_ylabel(r'max. diam. [$\mathrm{\AA}$]', fontsize=16)
+ax[1].legend(fontsize=16)
 fig.tight_layout()
 fig.savefig(
     'inflation_example.pdf',
