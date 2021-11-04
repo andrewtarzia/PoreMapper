db7159f0955d9162e965c4a66e271376a4c9c61d
Andrew Tarzia <andrew.tarzia@gmail.com>
2021-11-04 18:52:13 +0000

    Add final result.

--- roll_gather/__init__.py
+++ roll_gather/__init__.py
@@ -3,7 +3,7 @@ from .host import *  # noqa
 from .bead import *  # noqa
 from .blob import *  # noqa
 from .pore import *  # noqa
-from .step_result import *  # noqa
+from .result import *  # noqa
 from .utilities import *  # noqa
 from .inflater import *  # noqa
 from .radii import *  # noqa
---
+++ roll_gather/result.py
@@ -0,0 +1,30 @@
+"""
+Result
+======
+
+#. :class:`.InflationResult`
+#. :class:`.InflationStepResult`
+
+"""
+
+from dataclasses import dataclass
+
+from .blob import Blob
+from .pore import Pore
+
+
+@dataclass(frozen=True)
+class InflationResult:
+    step: int
+    num_movable_beads: float
+    blob: Blob
+    pore: Pore
+    total_pore: Pore
+
+
+@dataclass(frozen=True)
+class InflationStepResult:
+    step: int
+    num_movable_beads: float
+    blob: Blob
+    pore: Pore
